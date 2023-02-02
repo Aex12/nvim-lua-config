4b40722dbd7de12ade70627ee898442987f4e2ef
Aex12 <alexcastonmolero@gmail.com>
2023-02-02 23:56:13 +0100

    add file explorer

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -43,3 +43,7 @@ nnoremap("<leader>ff", ":Telescope find_files<CR>")
 nnoremap("<leader>fg", ":Telescope live_grep<CR>")
 nnoremap("<leader>fb", ":Telescope buffers<CR>")
 nnoremap("<leader>fh", ":Telescope help_tags<CR>")
+
+nnoremap('<leader>tt', ':NvimTreeFocus<CR>')
+nnoremap('<leader>tf', ':NvimTreeFindFile<CR>')
+nnoremap('<leader>tc', ':NvimTreeCollapse<CR>')
