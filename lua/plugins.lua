ca0622ebf73161f5c3e18a6c5d69fc6cc4bfd2c8
Aex12 <alexcastonmolero@gmail.com>
2023-02-06 21:43:44 +0100

    add again nvim treesitter textobjects

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,8 +27,74 @@ return require('lazy').setup({
       matchup = {
         enable = true,
       },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['aa'] = '@parameter.outer',
+            ['ia'] = '@parameter.inner',
+            ['ab'] = '@block.outer',
+            ['ib'] = '@block.inner',
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+            ['ak'] = '@call.outer',
+            ['ik'] = '@call.inner',
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["gnF"] = "@function.outer",
+            ["gnC"] = "@class.outer",
+            ["gnS"] = "@scope",
+            ["gnB"] = "@block.outer",
+          },
+          goto_next_end = {
+            ["gnf"] = "@function.outer",
+            ["gnc"] = "@class.outer",
+            ["gns"] = "@scope",
+            ["gnb"] = "@block.outer",
+          },
+          goto_previous_start = {
+            ["gNF"] = "@function.outer",
+            ["gNC"] = "@class.outer",
+            ["gNS"] = "@scope",
+            ["gNB"] = "@block.outer",
+          },
+          goto_previous_end = {
+            ["gNf"] = "@function.outer",
+            ["gNc"] = "@class.outer",
+            ["gNs"] = "@scope",
+            ["gNb"] = "@block.outer",
+          },
+          -- Below will go to either the start or the end, whichever is closer.
+          -- Use if you want more granular movements
+          -- Make it even more gradual by adding multiple queries and regex.
+          -- goto_next = {
+            -- ["]d"] = "@conditional.outer",
+          -- },
+          -- goto_previous = {
+            -- ["[d"] = "@conditional.outer",
+          -- }
+        },
+      },
     } end
   },
+  'nvim-treesitter/nvim-treesitter-textobjects',
 
   -- commentator
   {
