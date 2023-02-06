2c4a21771cdb2341a54946f8a76eff68adc2e0b8
Aex12 <alexcastonmolero@gmail.com>
2023-02-06 19:33:33 +0100

    remove treesitter-textobjects

--- lua/keymap/keymap.lua
+++ lua/keymap/keymap.lua
@@ -3,7 +3,7 @@ local responsiveTelescope = require('appearance.responsive-telescope')
 local telescope_ok, telescope = pcall(require, 'telescope.builtin')
 local cmp_ok, cmp = pcall(require, 'cmp')
 local luasnip_ok, luasnip = pcall(require, 'luasnip')
-local ts_repeat_move_ok, ts_repeat_move = pcall(require, 'nvim-treesitter.textobjects.repeatable_move')
+-- local ts_repeat_move_ok, ts_repeat_move = pcall(require, 'nvim-treesitter.textobjects.repeatable_move')
 
 -- import utils for mapping keys
 
@@ -65,18 +65,18 @@ local keymap = {
       vim.keymap.set('n', '<leader>fh', responsiveTelescope(telescope.help_tags), opts)
     end
 
-    if ts_repeat_move_ok then
-      -- Repeat movement with ; and ,
-      -- ensure ; goes forward and , goes backward regardless of the last direction
-      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_next)
-      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_previous)
-
-      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
-    end
+    -- if ts_repeat_move_ok then
+    --   -- Repeat movement with ; and ,
+    --   -- ensure ; goes forward and , goes backward regardless of the last direction
+    --   vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_next)
+    --   vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_previous)
+    --
+    --   -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+    --   vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+    --   vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+    --   vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+    --   vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+    -- end
 
     nnoremap('<leader>tt', ':Neotree toggle<CR>')
     nnoremap('<leader>tf', ':Neotree reveal<CR>')
