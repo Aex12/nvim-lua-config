5e695e1d2adf23ea6c96fbb5939a6ad1d8f48563
Aex12 <alexcastonmolero@gmail.com>
2023-02-18 23:25:17 +0100

    tree sitter based folding

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,93 +6,98 @@ return require('lazy').setup({
   -- TreeSitter based highlighting.
   {
     'nvim-treesitter/nvim-treesitter', run = ':TSUpdate',
-    config = function() require'nvim-treesitter.configs'.setup {
-      ensure_installed = {
-        'c', 'vim', 'help', 'dockerfile',
-        'diff', 'gitcommit', 'gitignore',
-        'bash', 'perl', 'lua', 'python',
-        'yaml', 'json', 'jsdoc',
-        'javascript', 'typescript', 'tsx', 'css', 'regex',
-        'prisma', 'sql',
-        'html', 'markdown', 'markdown_inline',  'php',
-        'go', 'dot', 'rust', 'smali'
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      context_commentstring = {
-        enable = true
-      },
-      matchup = {
-        enable = true,
-      },
-      textobjects = {
-        select = {
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+          'c', 'vim', 'help', 'dockerfile',
+          'diff', 'gitcommit', 'gitignore',
+          'bash', 'perl', 'lua', 'python',
+          'yaml', 'json', 'jsdoc',
+          'javascript', 'typescript', 'tsx', 'css', 'regex',
+          'prisma', 'sql',
+          'html', 'markdown', 'markdown_inline',  'php',
+          'go', 'dot', 'rust', 'smali'
+        },
+        auto_install = true,
+        highlight = {
           enable = true,
-          lookahead = true,
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-            ['ab'] = '@block.outer',
-            ['ib'] = '@block.inner',
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-            ['ak'] = '@call.outer',
-            ['ik'] = '@call.inner',
+        },
+        context_commentstring = {
+          enable = true
+        },
+        matchup = {
+          enable = true,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['ab'] = '@block.outer',
+              ['ib'] = '@block.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+              ['ak'] = '@call.outer',
+              ['ik'] = '@call.inner',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>A"] = "@parameter.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["gnF"] = "@function.outer",
+              ["gnC"] = "@class.outer",
+              ["gnS"] = "@scope",
+              ["gnB"] = "@block.outer",
+            },
+            goto_next_end = {
+              ["gnf"] = "@function.outer",
+              ["gnc"] = "@class.outer",
+              ["gns"] = "@scope",
+              ["gnb"] = "@block.outer",
+            },
+            goto_previous_start = {
+              ["gNF"] = "@function.outer",
+              ["gNC"] = "@class.outer",
+              ["gNS"] = "@scope",
+              ["gNB"] = "@block.outer",
+            },
+            goto_previous_end = {
+              ["gNf"] = "@function.outer",
+              ["gNc"] = "@class.outer",
+              ["gNs"] = "@scope",
+              ["gNb"] = "@block.outer",
+            },
+            -- Below will go to either the start or the end, whichever is closer.
+            -- Use if you want more granular movements
+            -- Make it even more gradual by adding multiple queries and regex.
+            -- goto_next = {
+              -- ["]d"] = "@conditional.outer",
+            -- },
+            -- goto_previous = {
+              -- ["[d"] = "@conditional.outer",
+            -- }
           },
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>a"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["gnF"] = "@function.outer",
-            ["gnC"] = "@class.outer",
-            ["gnS"] = "@scope",
-            ["gnB"] = "@block.outer",
-          },
-          goto_next_end = {
-            ["gnf"] = "@function.outer",
-            ["gnc"] = "@class.outer",
-            ["gns"] = "@scope",
-            ["gnb"] = "@block.outer",
-          },
-          goto_previous_start = {
-            ["gNF"] = "@function.outer",
-            ["gNC"] = "@class.outer",
-            ["gNS"] = "@scope",
-            ["gNB"] = "@block.outer",
-          },
-          goto_previous_end = {
-            ["gNf"] = "@function.outer",
-            ["gNc"] = "@class.outer",
-            ["gNs"] = "@scope",
-            ["gNb"] = "@block.outer",
-          },
-          -- Below will go to either the start or the end, whichever is closer.
-          -- Use if you want more granular movements
-          -- Make it even more gradual by adding multiple queries and regex.
-          -- goto_next = {
-            -- ["]d"] = "@conditional.outer",
-          -- },
-          -- goto_previous = {
-            -- ["[d"] = "@conditional.outer",
-          -- }
-        },
-      },
-    } end
+      }
+      vim.cmd [[set foldmethod=expr]]
+      vim.cmd [[set foldexpr=nvim_treesitter#foldexpr()]]
+      vim.cmd [[set nofoldenable]]
+    end
   },
   'nvim-treesitter/nvim-treesitter-textobjects',
 
